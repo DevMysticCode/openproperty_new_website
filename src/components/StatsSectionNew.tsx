91d5704c57149dbd348e075a2fdefd27db485208
Bunty1998 <pradeep.yadav.ick@gmail.com>
2025-09-23 23:43:19 +0530

    stats background imnage fix

--- src/components/StatsSectionNew.tsx
+++ src/components/StatsSectionNew.tsx
@@ -15,7 +15,7 @@ export default function StatsSectionNew({ delay = 0 }: StatsSectionProps) {
         decoding="async"
         className="absolute left-0 top-0 z-10 h-full w-full object-fill"
         style={{ color: 'transparent' }}
-        src="/public/default_glow.webp"
+        src="/default_glow.webp"
       />
 
       {/* Gradient overlay */}
