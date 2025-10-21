49f6a63b56b1be02a6ff99b5f3310b743a0f5779
Bunty1998 <pradeep.yadav.ick@gmail.com>
2025-10-21 20:55:25 +0530

    padding changed as suggested by alex updates

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -144,7 +144,7 @@ export default function Hero() {
         <div className="absolute bottom-0 left-0 right-0 h-32 bg-gradient-to-t from-black via-black/50 to-transparent"></div>
       </section>
 
-      <section className=" w-full">
+      <section className="w-full">
         <ScrollProgressBar />
         {/* Mission Section - Below the main content */}
         <motion.div
